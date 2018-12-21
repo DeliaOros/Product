26838af532ac2e5e440c6819ea6e592560e1d0af
DeliaOros <delia_iletchi@yahoo.com>
2018-12-21 17:29:43 +0200

    message

---
+++ Products/Product.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Products
+{
+    public class Product
+    {
+        private string name;
+        private float weight;
+        private string producer;
+        public enum Categories { Dairy = 1, Fruit, Beverages, Bread };
+        private readonly Categories productCategory;
+        private DateTime productionDate;
+        private readonly DateTime expirationDate;
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+
+        public float Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+
+
+        public string Producer
+        {
+            get
+            {
+                return producer;
+            }
+            set
+            {
+                producer = value;
+            }
+        }
+
+
+        public Categories ProductCategory
+        {
+            get { return productCategory; }
+        }
+
+
+        public DateTime ExpirationDate
+        {
+            get
+
+            {
+                if (expirationDate > DateTime.Now)
+                {
+                    return expirationDate;
+                }
+                else
+                {
+                    return DateTime.MinValue;
+                }
+
+            }
+
+        }
+        public Product(Categories category, int productionYear, int productionMonth, int productionDay, int productionHour, int productionSecond, int productionMilisecond)
+        {
+            this.productionDate = new DateTime(productionYear, productionMonth, productionDay, productionHour, productionSecond, productionMilisecond);
+            this.productCategory = category;
+            switch (productCategory)
+            {
+                case Categories.Beverages:
+                    expirationDate = productionDate.AddYears(3);
+                    break;
+                case Categories.Bread:
+                    expirationDate = productionDate.AddDays(4);
+                    break;
+                case Categories.Dairy:
+                    expirationDate = productionDate.AddDays(7);
+                    break;
+                case Categories.Fruit:
+                    expirationDate = productionDate.AddDays(2);
+                    break;
+                default:
+                    expirationDate = productionDate.AddYears(-10);
+                    break;
+            }
+            if (expirationDate < DateTime.Now)
+            {
+                Console.WriteLine("WARNING! The product expired at {0}, withdraw it from consumption and report event!", expirationDate.ToString());
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("This product is {0} from {1} category and weights {2} kg. It expires at {3}."
+                                , this.Name.ToString().ToUpper(), this.ProductCategory.ToString(),
+                                this.Weight.ToString(), this.ExpirationDate.ToString());
+        }
+
+    }
+}
---
+++ Products/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(Product.Categories.Fruit, 2018, 12, 04, 3, 21, 14);
+            product1.Name = "apple";
+            product1.Producer = "Livada";
+            product1.Weight = 0.50f;
+            Console.WriteLine(product1.ToString());
+
+            Console.WriteLine("----------------------------------------------------------------------------------");
+           
+            Product product2 = new Product(Product.Categories.Beverages, 2017, 3, 2, 3, 44, 8);
+            product2.Name = "coffee";
+            product2.Producer = "Columbia";
+            product2.Weight = .250f;
+            Console.WriteLine(product2.ToString());
+
+            Console.WriteLine("----------------------------------------------------------------------------------");
+
+
+            Product product3 = new Product(Product.Categories.Dairy, 2018, 2, 3, 5, 6, 7);
+            product3.Name = "coffee";
+            product3.Producer = "Columbia";
+            product3.Weight = .250f;
+            Console.WriteLine(product3.ToString());
+
+            Console.WriteLine("----------------------------------------------------------------------------------");
+
+        }
+    }
+}
